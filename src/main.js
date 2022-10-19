3a74c6b87571138f3ec42d055e594c96022984dd
rick-silva <ricksant.1@hotmail.com>
2022-10-19 16:03:07 -0300

    aula 1

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import "./css/index.css"
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
-
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
 
 
@@ -16,6 +16,9 @@ const setCardType = (type) => {
         default: ["black", "gray"],
     }
 
-    ccBgColor01.setAttribute("fill", "green")
-    ccBgColor02.setAttribute("fill", "blue")
+    ccBgColor01.setAttribute("fill", colors[type][0])
+    ccBgColor02.setAttribute("fill", colors[type][1])
+    ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
+
+setCardType("default")
